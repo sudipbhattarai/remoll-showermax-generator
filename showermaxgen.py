7ae6051317b25aecb66e90548fb93498cff4fe10
sudipbhattarai <bhatsudi@isu.edu>
2023-05-23 15:58:12 -0600

    Added reflective mylar wrap around the quartz

--- showermaxgen.py
+++ showermaxgen.py
@@ -19,13 +19,19 @@ width_tungsten = width_quartz
 thick_tungsten = 8.0
 
 ## Spacer in TQ stack
-thick_spacer = 0.1
+thick_spacer = 0.3
 thick_tolerance = 0.0
 
 ## Tungsten-quartz stack
 length_stack_tungstenquartz = length_quartz
 width_stack_tungstenquartz = width_quartz
 thick_stack_tungstenquartz = 4*(thick_quartz+thick_tungsten)+ 8*thick_spacer + 2*thick_tolerance
+
+# Mylar wrapping
+thick_wrap_film = 0.075
+length_wrap = length_quartz + 2*thick_wrap_film
+width_wrap = width_quartz
+thick_wrap = thick_quartz + 4*thick_wrap_film
 
 ## Mirror box bottom (lower part of the light guide)
 length_mirror_box_bot = 67.462  
@@ -115,7 +121,7 @@ f=open(output_file+".gdml", "w+")
 out="<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n\n"
 
 out+="<!DOCTYPE gdml [\n"
-out+="\t<!ENTITY matrices SYSTEM \"matrices.xml\">\n"
+out+="\t<!ENTITY matrices SYSTEM \"showerMaxMatrices.xml\">\n"
 out+="]>\n\n"
 
 out+="<gdml"
@@ -203,6 +209,17 @@ out+="\t</xtru>\n"
 
 out+="\t<box name=\"solid_tungsten\" lunit=\"mm\" x=\""+str(length_tungsten)+"\" y=\""+str(width_tungsten)+"\" z=\""+str(thick_tungsten)+"\"/>\n"
 
+out+="\t<box name=\"solid_wrap_1\" lunit=\"mm\" x=\""+str(length_wrap)+"\" y=\""+str(width_wrap)+"\" z=\""+str(thick_wrap)+"\"/>\n"
+out+="\t<box name=\"solid_wrap_2\" lunit=\"mm\" x=\""+str(length_wrap+0.1)+"\" y=\""+str(width_wrap+1.0)+"\" z=\""+str(thick_wrap-2*thick_wrap_film)+"\"/>\n"
+
+out+="\t<subtraction name=\"solid_wrap\">"
+out+="\n\t\t<first ref=\"solid_wrap_1\"/>"
+out+="\n\t\t<second ref=\"solid_wrap_2\"/>"
+out+="\n\t\t<position name=\"pos_subtract_wrap_12\" x=\""+str(thick_wrap_film)+"\" y=\"0\" z=\"0\"/>"
+out+="\n\t\t<rotation name=\"rot_subtract_wrap_12\" x=\"0\" y=\"0\" z=\"0\"/>"
+out+="\n\t</subtraction>\n"
+#-------------------
+
 #out+="\t<tube name=\"solid_pmt_filter\" rmin=\"0\" rmax=\""+str(radius_pmt)+"\" z=\""+str(length_pmt_filter)+"\" deltaphi=\"2*pi\" startphi=\"0\" aunit=\"rad\" lunit=\"mm\"/>\n"
 
 out+="\t<tube name=\"solid_pmt_window\" rmin=\"0\" rmax=\""+str(radius_pmt)+"\" z=\""+str(length_pmt_window)+"\" deltaphi=\"2*pi\" startphi=\"0\" aunit=\"rad\" lunit=\"mm\"/>\n"
@@ -217,6 +234,11 @@ out+="\t<box name=\"solid_showerMaxMother\" lunit=\"mm\" x=\""+str(450)+"\" y=\"
 ## Optical Surfaces
 out+="\n\t<opticalsurface name=\"quartz_surface\" model=\"glisur\" finish=\"ground\" type=\"dielectric_dielectric\" value=\"0.98\" >\n"
 out+="\t</opticalsurface>\n"
+
+out+="\t<opticalsurface name=\"mylar_surface\" model=\"glisur\" finish=\"polished\" type=\"dielectric_metal\" value=\"1.0\" >\n"
+out+="\t\t<property name=\"REFLECTIVITY\" ref=\"Mylar_REFLECTIVITY_30DEG\" />\n"
+out+="\t</opticalsurface>\n"
+
 out+="\t<opticalsurface name=\"Cathode_surface\" model=\"glisur\" finish=\"polished\" type=\"dielectric_metal\" value=\"1.0\">\n"
 out+="\t\t<property name=\"REFLECTIVITY\" ref=\"CathodeSurf_REFLECTIVITY\" />\n"
 out+="\t\t<property name=\"EFFICIENCY\" ref=\"Cathode_EFFICIENCY\" />\n"
@@ -241,6 +263,12 @@ for i in range(0,nSMmodules):
                 #out+="\n\t\t<auxiliary auxtype=\"Color\" auxvalue=\"red\"/>"
                 out+="\n\t</volume>\n"
 
+                out+="\t<volume name=\"logic_wrap_"+str(i)+"_"+str(j)+"\">"
+                out+="\n\t\t<materialref ref=\"Aluminum_material\"/>"
+                out+="\n\t\t<solidref ref=\"solid_wrap\"/>"
+                #out+="\n\t\t<auxiliary auxtype=\"Color\" auxvalue=\"blue\"/>"
+                out+="\n\t</volume>\n"
+      
         #out+="\t<volume name=\"logic_pmt_filter_"+str(i)+"\">"
         #out+="\n\t\t<materialref ref=\"Quartz\"/>"
         #out+="\n\t\t<solidref ref=\"solid_pmt_filter\"/>"
@@ -302,6 +330,12 @@ for i in range(0,nSMmodules):
                 out+="\n\t\t\t<rotation name=\"rot_logic_tungsten_"+str(i)+"_"+str(j)+"\" x=\"0\" y=\"0\" z=\"0\"/>"
                 out+="\n\t\t</physvol>"
 
+                out+="\n\t\t<physvol name=\"wrap_"+str(i)+"_"+str(j)+"\">"
+                out+="\n\t\t\t<volumeref ref=\"logic_wrap_"+str(i)+"_"+str(j)+"\"/>"             
+                out+="\n\t\t\t<position name=\"pos_logic_wrap_"+str(i)+"_"+str(j)+"\" x=\""+str(0)+"\" y=\""+str(0)+"\" z=\""+str(1.5*thick_quartz+2.0*thick_tungsten+4*thick_spacer+thick_tolerance-j*(thick_quartz+thick_tungsten)-(2*j+1)*thick_spacer)+"\"/>"
+                out+="\n\t\t\t<rotation name=\"rot_logic_wrap_"+str(i)+"_"+str(j)+"\" x=\"0\" y=\"0\" z=\"0\"/>"        
+                out+="\n\t\t</physvol>"
+
         out+="\n\t\t<auxiliary auxtype=\"Alpha\" auxvalue=\"0.0\"/>"
         out+="\n\t</volume>\n"
             
@@ -334,6 +368,11 @@ out+="\t</skinsurface>\n"
 #out+="\t<skinsurface name=\"mirror_box_bottom_skin_surface\" surfaceproperty=\"Al_mirror_surface\" >\n"
 #out+="\t\t<volumeref ref=\"logic_mirror_box_bot_"+str(i)+"\"/>\n"
 #out+="\t</skinsurface>\n"
+for iMod in range(0,nSMmodules):
+    for iWrap in range(0,nQuartz):
+        out+="\t<skinsurface name=\"wrap_skin_surface_"+str(iMod)+"_"+str(iWrap)+"\" surfaceproperty=\"mylar_surface\" >\n"
+        out+="\t\t<volumeref ref=\"logic_wrap_"+str(iMod)+"_"+str(iWrap)+"\"/>\n"
+        out+="\t</skinsurface>\n"
 out+="\t<bordersurface name=\"cathode_window_border_surface\" surfaceproperty=\"Cathode_surface\" >\n"
 out+="\t<physvolref ref=\"pmt_window_"+str(i)+"\"/>\n"
 out+="\t<physvolref ref=\"pmt_cathode_"+str(i)+"\"/>\n"
